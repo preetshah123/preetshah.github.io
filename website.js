5f7b6e4d94d9b651cea4a01f55340dc0833db6df
preetshah123 <preettshah@hotmail.com>
2019-01-22 18:01:36 -0500

    Reorganized file structure

---
+++ website.js
@@ -0,0 +1,29 @@
+$(window).scroll(function(){
+    $(".slideanim").each(function(){
+        var pos=$(this).offset().top;
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600){
+            $(this).addClass("slide");
+        }
+    });
+});   
+    
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});    
+
+$(document).ready(function(){
+    $(".navbar a, footer a[href='#myPage']").on('click',function(event){
+        if(this.hash !==""){
+            event.preventDefault();
+            var hash =this.hash;
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top
+            },900, function(){
+                window.location.hash=hash;
+            });
+        }
+    });
+});
